80b30eee2b853db485aea4dadd2245b56d22b8ad
Ballofmoss <ivansvenn@ya.ru>
2024-03-27 13:31:46 +0400

    ура создал проект

---
+++ Assets/Settings.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    bool isFullScreen = false;
+    public void FullScreenToggle()
+    {
+
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
+    
+}
